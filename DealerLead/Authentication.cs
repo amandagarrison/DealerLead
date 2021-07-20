8411056dcd49a5be2bb3242ce9c6652237ac7cfc
Amanda <amanda.garrison@improving.com>
2021-07-20 16:17:14 -0500

    grabbing user oid, need to pass into db

---
+++ DealerLead/Authentication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+namespace DealerLead
+{
+    class Authentication
+    {
+        private async Task OnTokenValidatedFunc(TokenValidatedContext context)
+        {
+            // Custom code here
+            await Task.CompletedTask.ConfigureAwait(false);
+        }
+    }
+}
